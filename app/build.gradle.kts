d241d4b70da8672bf0a1a1fd91f7e79fc1c8e3ef
valery.bokov <vlry.bkv@gmail.com>
2025-11-07 22:39:57 +0100

    add basic version of App bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.navigation.compose)
 
     implementation(libs.hilt.android)
--- app/src/main/java/com/example/scratchcardexample/MainActivity.kt
+++ app/src/main/java/com/example/scratchcardexample/MainActivity.kt
@@ -7,15 +7,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.scratchcardexample.feature.startscreen.StartScreen
 import com.example.scratchcardexample.ui.theme.ScratchCardExampleTheme
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.compose.runtime.State
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -56,4 +66,28 @@ private fun AppNavHost() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AppBar(
+    titleId: State<Int>,
+    onBack: () -> Unit,
+    showBackButton: Boolean) {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(titleId.value))
+        },
+        navigationIcon = {
+            if (showBackButton) {
+                IconButton(onClick = onBack) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.back),
+                        tint = MaterialTheme.colorScheme.onBackground,
+                    )
+                }
+            }
+        }
+    )
 }
